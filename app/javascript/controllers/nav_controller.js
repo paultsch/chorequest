938c518a3a7ca128617802e57bb28ee6748eec8f
Paul Tschudin <paul.tschudin@gmail.com>
2026-02-23 14:56:12 -0600

    Fix nav: hide mobile menus by default and toggle all menu targets

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -5,15 +5,15 @@ export default class extends Controller {
 
   connect() {
     if (this.hasMenuTarget) {
-      this.menuTarget.classList.add('hidden')
+      this.menuTargets.forEach(m => m.classList.add('hidden'))
     }
   }
 
   toggle(event) {
     event.preventDefault()
     if (!this.hasMenuTarget || !this.hasButtonTarget) return
-    this.menuTarget.classList.toggle('hidden')
-    const expanded = this.menuTarget.classList.contains('hidden') ? 'false' : 'true'
+    this.menuTargets.forEach(m => m.classList.toggle('hidden'))
+    const expanded = this.menuTargets[0].classList.contains('hidden') ? 'false' : 'true'
     this.buttonTarget.setAttribute('aria-expanded', expanded)
   }
 }
